1a3d9d5c4768850c1591e2f4d01ae2ff570ed7ab
vnknt <kanatvn@gmail.com>
2021-12-06 16:56:18 +0300

    First commit

---
+++ src/main/java/com/vnkanat/ecommercemicroservice/EcommerceMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.vnkanat.ecommercemicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vnkanat/ecommercemicroservice/EcommerceMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vnkanat.ecommercemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
